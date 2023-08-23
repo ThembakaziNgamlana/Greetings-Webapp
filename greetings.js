74ac62574821ade8d56593c3ea1992fb83b70e82
Thembakazi <thembakazingamlana25@gmail.com>
2023-08-23 21:42:30 +0200

    ädding changes

--- greetings.js
+++ greetings.js
@@ -3,6 +3,7 @@ export default function createGreetingApp() {
   let lastGreetedName = {};
   let greetedNames = {};
  let validationMessage ='';
+ let greeting = '';
 
   function incrementPeopleNamesCount(selectedName) {
       if ( !greetedNames[selectedName]) {
@@ -40,36 +41,45 @@ function greetFunction(){
   
 
   function getGreetingMessage(selectedLanguage, selectedName) {
-      let greeting = '';
-      if (selectedLanguage === 'english') {
+      
+      if(selectedLanguage && selectedName){
+        //incrementPeopleNamesCount(selectedName)
+        if (selectedLanguage === 'english') {
           greeting = `Hello, ${selectedName}`;
       } else if (selectedLanguage === 'isixhosa') {
           greeting = `Molo, ${selectedName}`;
       } else if (selectedLanguage === 'french') {
           greeting = `Bonjour, ${selectedName}`;
       }
-      return greeting;
+     
   }
+      }
+       
+    function displayGreetings(){
+      return greeting;
+    }
 
 
   function handleGreetBtnClick(selectedName, selectedLanguage) {
-  if (!selectedName && !selectedLanguage) {
-    return ('Please enter a name and select a language.');
   
-    }
-
-    else if  (!selectedName) {
-      return ('Please enter a name.');
-      
-    }
-
-    else if (!selectedLanguage) {
-      return ('Please select a language.');
-      
-    }
+    if (!selectedName && selectedLanguage == null) {
+      return "Please enter name and select language."
+  }
+  else if (selectedLanguage == null) {
+      return "Language not selected."
+  }
+else if (!selectedName) {
+      return "Please enter your name."
+  }
+    
   }
 
-
+  // function displayMessage(message) {
+  //   validationMessage = message; 
+  //   setTimeout(() => {
+  //     validationMessage = ''; 
+  //   }, 2000);
+  // }
 
   return {
       incrementPeopleNamesCount,
@@ -79,6 +89,8 @@ function greetFunction(){
      greetFunction,
     getGreetCountForUser,
     reset,
+    displayGreetings,
+    //displayMessage,
     
     
   };
--- index.js
+++ index.js
@@ -1,160 +1,108 @@
 import express from 'express';
 import exphbs from 'express-handlebars';
 import bodyParser from 'body-parser';
+import pgPromise from 'pg-promise';
 import createGreetingApp from './greetings.js';
-
-
-
-
 const app = express();
 const greet = createGreetingApp();
-
 const hbs = exphbs.create({
     extname: '.hbs',
     defaultLayout: false,
     layoutDir: "./views/layouts",
 });
-
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
-
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
-
 app.set('views', './views');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-
-app.get('/', function (req, res) {
-    res.render('index');
+const pgp = pgPromise();
+const db = pgp({
+    connectionString: 'postgres://greetings_56o5_user:j33VLLfyHvKsIRPszuI2bhsZRb2OIyY9@dpg-cjhkunb6fquc73d5n9d0-a.oregon-postgres.render.com:5432/greetings_56o5',
+    ssl: { rejectUnauthorized: false }
 });
 
- app.get('/greetings', (req, res) => {
-   res.render('index', { greetedCount: greet.getNameCount() });
-
- });
-
- app.get('/greetings', (req, res) => {
- res.render('greetings', {
-       greetedCount: greet.getNameCount(),
-      validationMessage: ''
-    });
+app.get('/', (req, res) => {
+  console.log(greet.getNameCount())
+    res.render('index', {
+        greeting: greet.displayGreetings(),
+        greetedCounter: greet.getNameCount(),
+        validationMessage: greet.handleGreetBtnClick()
+    })
 });
+
+app.post('/namesTrack/:userId', async (req, res) => {
+    const userId = req.params.userId;
+    const { language, newName } = req.body;
+    console.log(`New name: ${newName}, Language: ${language}`); // Log the entered data to the terminal
+    const updateQuery = 'UPDATE users SET language = $1, name = $2 WHERE id = $3';
+    await db.none(updateQuery, [language, newName, userId]);
+    res.redirect('/');
+});
+
+
+app.get('/greetings', (req, res) => {
+    res.render('index', { greetedCount: greet.getNameCount() });
+});
+
 app.get('/greetAction', function (req, res) {
     const greetedNames = greet.greetFunction();
     res.render('greetAction', { greetedNames, greet });
-  });
+})
 
-
-//   app.get('/userCount/:userName', (req, res) => {
-//     const userName = req.params.userName;
-//     const greetCountForUser = greet.getGreetCountForUser(userName);
-
-//     res.json({userName, greetCountForUser });
-// });
 app.get('/userCount/:userName', (req, res) => {
     const userName = req.params.userName;
     const greetCountForUser = greet.getGreetCountForUser(userName);
-  
     const greetMessage = `${userName} has been greeted ${greetCountForUser} times.`;
     res.send(greetMessage);
-  });
-
+});
 
 app.post('/reset', function (req, res) {
     greet.reset();
-  
     res.render('index', {
-      validationMessage: '', 
-      greetedCount: 0,
-      action: '' 
+        validationMessage: '',
+        greetedCount: 0,
+        action: ''
     });
-  });
-
-
-// app.post('/greetings', (req, res) => {
-//     const name = req.body.name;
-//     const language = req.body.language; 
-
-//     const validationMessage = greet.handleGreetBtnClick(name, language); 
-
-//     const greetingMessage = greet.getGreetingMessage(language, name);
-    
-//     if (validationMessage) { 
-//         res.render('index', {
-//             greetedCount: greet.getNameCount(),
-//             validationMessage,
-//             greetingMessage: ''
-//         });} 
-//         // else {
-//     //     greet.incrementPeopleNamesCount(name);
-//     //     res.render('index', {
-//     //         greetingMessage,
-//     //         greetedCount: greet.getNameCount()
-//     //     });
-//     // }
-
-// // if (name && language) {
-// //         greet.incrementPeopleNamesCount(name);
-// //     }
-// else {
-//                 res.render('index', {
-//                     validationMessage: 'User has already been greeted.',
-//                     greetedCount: greet.getNameCount(),
-//                     greetingMessage: ''
-//                 });
-//             }
-    
-//     res.render('index', {greetingMessage, greetedCount: greet.getNameCount()});
-// });
-
-app.post('/greetings', (req, res) => {
-  const name = req.body.name;
-  const language = req.body.language; 
-
-  const validationMessage = greet.handleGreetBtnClick(name, language); 
-
-  const greetingMessage = greet.getGreetingMessage(language, name);
-  
-  if (validationMessage) { 
-      res.render('index', {
-          greetedCount: greet.getNameCount(),
-          validationMessage,
-          greetingMessage: ''
-      });
-  } else {
-      if (name && language) {
-          const greeted = greet.incrementPeopleNamesCount(name);
-          if (greeted) {
-              res.render('index', {
-                  greetingMessage,
-                  greetedCount: greet.getNameCount()
-              });
-          } else {
-              res.render('index', {
-                  validationMessage: 'User has already been greeted.',
-                  greetedCount: greet.getNameCount(),
-                  greetingMessage: ''
-              });
-          }
-      } else {
-          res.render('index', {
-              greetedCount: greet.getNameCount(),
-              validationMessage: 'Please enter a name and select a language.',
-              greetingMessage: ''
-          });
-      }
-  }
 });
 
+app.post('/greetings', (req, res) => {
+    const name = req.body.name;
+    const language = req.body.language;
+    // const validationMessage = greet.handleGreetBtnClick(name, language);
+    const greetingMessage = greet.getGreetingMessage(language, name);
+   greet.incrementPeopleNamesCount(name) 
 
-const PORT = process.env.PORT || 3011;
-
-
+}
+);
+const PORT = process.env.PORT || 3010;
 app.listen(PORT, () => {
     console.log('App started at port', PORT);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
