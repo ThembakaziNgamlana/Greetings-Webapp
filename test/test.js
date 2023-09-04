947c277a6190f4f51766003b6ec5ad1b56659fd6
Thembakazi <thembakazingamlana25@gmail.com>
2023-09-04 17:00:42 +0200

    ädding changes

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
 import { strict as assert } from 'assert';
 import pgPromise from 'pg-promise';
-import Greetdb from '../greetdb.js'; 
+import  greetingsDB from '../greetdb.js'; 
 
 const pgp = pgPromise();
 
 const connectionString = 'postgres://greetings_56o5_user:j33VLLfyHvKsIRPszuI2bhsZRb2OIyY9@dpg-cjhkunb6fquc73d5n9d0-a.oregon-postgres.render.com/greetings_56o5?ssl=true';
 const db = pgp(connectionString);
 
-const greetDb = Greetdb(db);
+const greetDb =  greetingsDB(db);
 
 describe('Greetdb functions', function() {
   this.slow(3000); 
@@ -24,12 +24,7 @@ describe('Greetdb functions', function() {
     assert.deepStrictEqual(result, [{ name: 'Themby' }]);
   });
 
-  it('updates the count for a name', async function() {
-    await greetDb.addName('Akhona');
-    await greetDb.updateCountForName('Akhona');
-    const result = await greetDb.getCountForName('Akhona');
-    assert.strictEqual(result[0].count, 2); 
-  });
+ 
 
   it('should retrieve names', async () => {
     await greetDb.addName('Zola');
